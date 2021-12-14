ca9f1c75d0afea3c3afbfbe81e4028b8e533b8c9
Aravindan G <aravindan.g9@gmail.com>
2021-12-14 21:25:42 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'onenotone';
+  arrayList: any[] = [
+    {content: 'content 1'},
+    {content: 'content 2'},
+    {content: 'content 3'},
+    {content: 'content 4'},
+    {content: 'content 5'},
+    {content: 'content 6'},
+    {content: 'content 7'},
+    {content: 'content 8'}
+    
+  ]
 }
